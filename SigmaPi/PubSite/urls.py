dde676e89d78108298d03630aafa6a5944167722
Christopher Botaish <cbotaish@wpi.edu>
2013-11-08 14:11:30 -0500

    Added basic login form

--- SigmaPi/PubSite/urls.py
+++ SigmaPi/PubSite/urls.py
@@ -3,7 +3,5 @@ from django.conf.urls import patterns, url
 from PubSite import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
-    url(r'^logout/', views.logout_view, name='logout_view'),
-    url(r'^login/', views.login_view, name='login_view')
+	url(r'', 'django.contrib.auth.views.login',name="login")
 )
