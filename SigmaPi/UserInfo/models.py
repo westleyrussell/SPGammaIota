40d68419ef0efda6ff250a881460cd825948195a
Mike Calder <mrcalder@wpi.edu>
2013-11-08 14:39:22 -0500

    Finish UserInfo Model

--- SigmaPi/UserInfo/models.py
+++ SigmaPi/UserInfo/models.py
@@ -1,18 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+def filepath(self, filename):
+	return ""
+
 # Create your models here.
 class UserInfo(models.Model):
 	user = models.OneToOneField(User)
-	picture = models.ImageField()
+	picture = models.FileField(upload_to=filepath)
 	phoneNumber = models.PositiveIntegerField()
 	graduationYear = models.PositiveIntegerField()
-	major = models.CharField(max_length=None)
-	hometown = models.CharField(max_length=None)
-	activities = models.CharField(max_length=None)
-	interests = models.CharField(max_length=None)
-	favoriteMemory = models.CharField(max_length=None)
-	bigBrother = models.CharField(max_length=None)
+	major = models.CharField(max_length=100)
+	hometown = models.CharField(max_length=100)
+	activities = models.CharField(max_length=200)
+	interests = models.CharField(max_length=200)
+	favoriteMemory = models.CharField(max_length=200)
+	bigBrother = models.ForeignKey(User, related_name="big_brother")
 
 	def __unicode__(self):
 		return "Info for " + __unicode__(self.user)
