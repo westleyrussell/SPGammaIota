c94db48847d033a629e98d53fe5ca13953c830fb
Christopher Botaish <cbotaish@wpi.edu>
2013-11-08 14:46:12 -0500

    Added user info to admin

--- SigmaPi/PubSite/urls.py
+++ SigmaPi/PubSite/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import patterns, url
 from PubSite import views
 
 urlpatterns = patterns('',
-	url(r'^login/', 'django.contrib.auth.views.login',name="login"),
-	url(r'^logout/', 'django.contrib.auth.views.logout_then_login',name="logout"),
+	url(r'^login', 'django.contrib.auth.views.login',name="login"),
+	url(r'^logout', 'django.contrib.auth.views.logout_then_login',name="logout"),
 	url(r'^', views.index, name="index"),
 
 )
