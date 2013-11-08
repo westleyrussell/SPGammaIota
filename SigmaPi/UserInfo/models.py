a1a4871c30a6fe9781f870450bc96967bc13ff08
Mike Calder <mrcalder@wpi.edu>
2013-11-08 14:27:22 -0500

    Model Fix

--- SigmaPi/UserInfo/models.py
+++ SigmaPi/UserInfo/models.py
@@ -4,9 +4,9 @@ from django.contrib.auth.models import User
 # Create your models here.
 class UserInfo(models.Model):
 	user = models.OneToOneField(User)
-	picture = models.ImageField([upload_to=None, height_field=None, width_field=None, max_length=100, **options])
-	phoneNumber = models.PositiveIntegerField([**options])
-	graduationYear = models.PositiveIntegerField([**options])
+	picture = models.ImageField()
+	phoneNumber = models.PositiveIntegerField()
+	graduationYear = models.PositiveIntegerField()
 	major = models.CharField(max_length=None)
 	hometown = models.CharField(max_length=None)
 	activities = models.CharField(max_length=None)
