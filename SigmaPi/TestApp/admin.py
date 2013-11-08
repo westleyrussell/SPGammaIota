0a050080a6cf89e23805879fa4da08ea22cdbf77
Christopher Botaish <cbotaish@wpi.edu>
2013-11-08 10:59:49 -0500

    Added example code

---
+++ SigmaPi/TestApp/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from TestApp.models import Status
+
+admin.site.register(Status)
--- SigmaPi/TestApp/views.py
+++ SigmaPi/TestApp/views.py
@@ -1,1 +1,30 @@
 # Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import RequestContext, loader
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from TestApp.models import Status
+from django
+
+def index(request):
+	context = RequestContext(request, {
+			'error_msg' : "Not Authenticated",
+			'form' : LoginForm()
+		})
+
+	if request.user.is_authenticated():
+		status_list = Status.objects.filter(user=request.user)
+		context = RequestContext(request, {
+			'status_list' : status_list,
+			})
+
+	return render(request, 'status/index.html', context)
+
+def logout_view(request):
+	logout(request)
+
+	return index(request)
+
+def login_view(request):
+	return request
