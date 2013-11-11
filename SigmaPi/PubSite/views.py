504251047c7e5f6cf63e5e12df5f10eeae403a09
Dan Robertson <danrobertson@drob.wifi.wpi.edu>
2013-11-11 12:07:44 -0500

    skeleton code for pubsite pages. Still need to include static file links

--- SigmaPi/PubSite/views.py
+++ SigmaPi/PubSite/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext
 
 # Create your views here.
 def index(request):
-	return HttpResponse("Home Page")
+	context = RequestContext(request,{
+		'motd': 'drob rules'
+	})
+	return render(request,'index.html',context)
