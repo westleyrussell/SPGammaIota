6fab171354438f5b64c4202dc0afc254fff68f42
Dan Robertson <danrobertson@drob.wifi.wpi.edu>
2013-11-18 16:11:01 -0500

    fluent

--- SigmaPi/static/js/pub/pub_common.js
+++ SigmaPi/static/js/pub/pub_common.js
@@ -25,13 +25,18 @@ $(document).ready(function(){
 	{
 		$('#nav-home').addClass('active');
 	}
+	$('.sub-header').css('margin-top','0px'); //can't do this in the css for some reason.
 
 	var thresh = 100; //The height of the top banner
+	var subHeight = $('#pub-nav').outerHeight();
 	$(window).scroll(function(e){
+		console.log(subHeight,window.scrollY);
 		if (window.scrollY > thresh) {
 			$('#pub-nav').addClass('fixed');
+			$('.sub-header').css('margin-top',subHeight + 'px');
 		} else {
 			$('#pub-nav').removeClass('fixed');
+			$('.sub-header').css('margin-top',0);
 		}
 	});
 });
