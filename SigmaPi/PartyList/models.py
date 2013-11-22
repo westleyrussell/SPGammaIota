a08f5e851334bf8087cc6c08b10dbeaaa3b7f070
Mike Calder <mrcalder@wpi.edu>
2013-11-22 16:34:11 -0500

    Sort of added a form to add a male guest
    Couldn't stop getting a 403 error when attempting to use the post
    request

--- SigmaPi/PartyList/models.py
+++ SigmaPi/PartyList/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django import forms
 
 class Party(models.Model):
 	"""
@@ -64,3 +65,9 @@ class PartyJob(models.Model):
 	class Meta:
 		verbose_name_plural = "Party Jobs"
 		verbose_name = "Party Job"
+
+
+
+
+class AddMaleGuestForm(forms.Form):
+    name = forms.CharField(max_length=100)
--- SigmaPi/PartyList/views.py
+++ SigmaPi/PartyList/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from PartyList.models import Party, PartyGuest, PartyJob
+from PartyList.models import Party, PartyGuest, PartyJob, AddMaleGuestForm
 from django.http import HttpResponse
 from django.template import RequestContext
+from datetime import datetime
 
 @login_required
 def parties(request):
@@ -22,11 +23,24 @@ def guests(request, party):
 		View for all guests on the list for a party
 	"""
 
+	if request.method == 'POST': # If the form has been submitted...
+		form = AddMaleGuestForm(request.POST) # A form bound to the POST data
+		if form.is_valid(): # All validation rules pass
+			name = form.cleaned_data['name']
+			g = Guest(name=name, birthDate=datetime.now(), gender="Male")
+			g.save()
+			return HttpResponseRedirect('/secure/parties/' + party + '/guests') # Redirect after POST
+	else:
+		form = AddMaleGuestForm() # An unbound form
+
+
 	requested_party = Party.objects.get(name__exact=party)
 	partyguests = PartyGuest.objects.filter(party=requested_party)
 	context = RequestContext(request, {
 			'partyname': party,
-			'partyguests': partyguests
+			'partyguests': partyguests,
+			'form': form,
+			'redirect': '/secure/parties/' + party + '/guests'
 		})
 	return render(request, 'partyguests.html', context)
 
@@ -43,3 +57,17 @@ def jobs(request, party):
 			'partyjobs': partyjobs
 		})
 	return render(request, 'partyjobs.html', context)
+
+def contact(request):
+    if request.method == 'POST': # If the form has been submitted...
+        form = ContactForm(request.POST) # A form bound to the POST data
+        if form.is_valid(): # All validation rules pass
+            # Process the data in form.cleaned_data
+            # ...
+            return HttpResponseRedirect('/thanks/') # Redirect after POST
+    else:
+        form = ContactForm() # An unbound form
+
+    return render(request, 'guests.html', {
+        'form': form,
+    })
