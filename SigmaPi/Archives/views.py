a4bfe6ca38a0c73e0216083338a7ecfe0884db37
Christopher Botaish <cbotaish@wpi.edu>
2014-01-09 18:53:46 -0500

    More work on archives.  Just need to add forms to create them and delete them now

--- SigmaPi/Archives/views.py
+++ SigmaPi/Archives/views.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from django.template import RequestContext
 from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+
+from Archives.models import Guide, MeetingMinutes, HouseRules, Bylaws
 
 
 @login_required
@@ -8,61 +12,87 @@ def index(request):
 	"""
 		View for the index page of the archives
 	"""
-	return render(request, "secure/archives_index.html", None)
+	return guides(request)#render(request, "secure/archives_index.html", None)
 
 @login_required
 def bylaws(request):
 	"""
 		View for all bylaws.
 	"""
-	return render(request, "secure/archives_bylaws.html", None)
+	bylaws = Bylaws.objects.all()
+	context = RequestContext(request, {
+		'bylaws': bylaws,
+		})
 
-@login_required
-def single_bylaw(request, year, month, day):
-	"""
-		View for a single bylaw
-	"""
-	return HttpResponse("Bylaws " + year + month + day)
+	return render(request, "secure/archives_bylaws.html", context)
+
+@permission_required('Archives.delete_bylaws', login_url='PubSite.views.permission_denied')
+def delete_bylaw(request, bylaw):
+	pass
+
+@permission_required('Archives.add_bylaws', login_url='PubSite.views.permission_denied')
+def create_bylaw(request):
+	pass
 
 @login_required
 def rules(request):
 	"""
 		View for all house rules
 	"""
-	return render(request, "secure/archives_rules.html", None)
 
-@login_required
-def single_rule(request, path):
-	"""
-		View for a single house rule
-	"""
-	return HttpResponse("Rule " + path)
+	rules = HouseRules.objects.all()
+	context = RequestContext(request, {
+		'rules': rules,
+		})
+
+	return render(request, "secure/archives_rules.html", context)
+
+@permission_required('Archives.delete_houserules', login_url='PubSite.views.permission_denied')
+def delete_rules(request, rules):
+	pass
+
+@permission_required('Archives.add_houserules', login_url='PubSite.views.permission_denied')
+def create_rules(request):
+	pass
 
 @login_required
 def minutes(request):
 	"""
 		View for all minutes
 	"""
-	return render(request, "secure/archives_minutes.html", None)
 
-@login_required
-def single_minutes(request, year, month, day):
-	"""
-		View for a single minutes
-	"""
-	return HttpResponse("Minutes " + year + month + day)
+	minutes = MeetingMinutes.objects.all()
+	context = RequestContext(request, {
+		'minutes': minutes,
+		})
+
+	return render(request, "secure/archives_minutes.html", context)
+
+@permission_required('Archives.delete_meetingminutes', login_url='PubSite.views.permission_denied')
+def delete_minutes(request, minutes):
+	pass
+
+@permission_required('Archives.add_meetingminutes', login_url='PubSite.views.permission_denied')
+def create_minutes(request):
+	pass
 
 @login_required
 def guides(request):
 	"""
 		View for all guides
 	"""
-	return render(request, "secure/archives_guides.html", None)
 
-@login_required
-def single_guide(request, path):
-	"""
-		View for a single guide
-	"""
-	return HttpResponse("Guide " + path)
+	guides = Guide.objects.all()
+	context = RequestContext(request, {
+		'guides': guides,
+		})
 
+	return render(request, "secure/archives_guides.html", context)
+
+@permission_required('Archives.delete_guide', login_url='PubSite.views.permission_denied')
+def delete_guide(request, guide):
+	pass
+
+@permission_required('Archives.add_guide', login_url='PubSite.views.permission_denied')
+def create_guide(request):
+	pass
