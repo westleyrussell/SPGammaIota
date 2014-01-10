11ee7b11b6d623936a56826f21d8a65190bbd232
Christopher Botaish <cbotaish@wpi.edu>
2014-01-09 19:16:15 -0500

    Added delete functionality.

--- SigmaPi/Archives/views.py
+++ SigmaPi/Archives/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.template import RequestContext
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import permission_required
+from django.utils.html import strip_tags
 
 from Archives.models import Guide, MeetingMinutes, HouseRules, Bylaws
 
@@ -27,8 +28,17 @@ def bylaws(request):
 	return render(request, "secure/archives_bylaws.html", context)
 
 @permission_required('Archives.delete_bylaws', login_url='PubSite.views.permission_denied')
-def delete_bylaw(request, bylaw):
-	pass
+def delete_bylaw(request):
+	"""
+		Deletes the bylaws with the given primary key.
+	"""
+
+	if request.method == 'POST':
+		bylaw_id = strip_tags(request.POST['post_id'])
+
+		post = Bylaws.objects.get(pk=bylaw_id)
+		post.delete()
+	return redirect('Archives.views.bylaws')
 
 @permission_required('Archives.add_bylaws', login_url='PubSite.views.permission_denied')
 def create_bylaw(request):
@@ -48,8 +58,17 @@ def rules(request):
 	return render(request, "secure/archives_rules.html", context)
 
 @permission_required('Archives.delete_houserules', login_url='PubSite.views.permission_denied')
-def delete_rules(request, rules):
-	pass
+def delete_rules(request):
+	"""
+		Deletes the rules with the given primary key.
+	"""
+
+	if request.method == 'POST':
+		rules_id = strip_tags(request.POST['post_id'])
+
+		post = HouseRules.objects.get(pk=rules_id)
+		post.delete()
+	return redirect('Archives.views.rules')
 
 @permission_required('Archives.add_houserules', login_url='PubSite.views.permission_denied')
 def create_rules(request):
@@ -69,8 +88,17 @@ def minutes(request):
 	return render(request, "secure/archives_minutes.html", context)
 
 @permission_required('Archives.delete_meetingminutes', login_url='PubSite.views.permission_denied')
-def delete_minutes(request, minutes):
-	pass
+def delete_minutes(request):
+	"""
+		Deletes the minutes with the given primary key.
+	"""
+
+	if request.method == 'POST':
+		minutes_id = strip_tags(request.POST['post_id'])
+
+		post = MeetingMinutes.objects.get(pk=minutes_id)
+		post.delete()
+	return redirect('Archives.views.minutes')
 
 @permission_required('Archives.add_meetingminutes', login_url='PubSite.views.permission_denied')
 def create_minutes(request):
@@ -90,8 +118,17 @@ def guides(request):
 	return render(request, "secure/archives_guides.html", context)
 
 @permission_required('Archives.delete_guide', login_url='PubSite.views.permission_denied')
-def delete_guide(request, guide):
-	pass
+def delete_guide(request):
+	"""
+		Deletes the guide with the given primary key.
+	"""
+
+	if request.method == 'POST':
+		guide_id = strip_tags(request.POST['post_id'])
+
+		post = Guide.objects.get(pk=guide_id)
+		post.delete()
+	return redirect('Archives.views.guides')
 
 @permission_required('Archives.add_guide', login_url='PubSite.views.permission_denied')
 def create_guide(request):
