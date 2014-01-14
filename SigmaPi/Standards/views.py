9e94d0d67c531056e524f5fc6744bfc74d8493cb
Christopher Botaish <cbotaish@wpi.edu>
2014-01-13 23:10:32 -0500

    Finished bone and social probation system.
    Just need to complete the pi points system.

--- SigmaPi/Standards/views.py
+++ SigmaPi/Standards/views.py
@@ -17,10 +17,11 @@ def index(request):
 		Displays the homepage of the standards module.
 	"""
 
-	bones = Bone.objects.filter(bonee=request.user)
-	current_bone_count = Bone.objects.filter(bonee=request.user, expirationDate__gt=datetime.now()).count()
+	current_bones = Bone.objects.filter(bonee=request.user, expirationDate__gt=datetime.now()).order_by('-expirationDate')
+	expired_bones = Bone.objects.filter(bonee=request.user, expirationDate__lte=datetime.now()).order_by('-expirationDate')
+	current_bone_count = current_bones.count()
 	
-	point_records = PiPointsRecord.objects.all().exclude(brother__groups__name='Alumni').order_by('points')
+	point_records = PiPointsRecord.objects.all().exclude(brother__groups__name='Alumni').order_by('-points')
 	
 	own_points = PiPointsRecord.objects.filter(brother=request.user)
 
@@ -34,14 +35,13 @@ def index(request):
 	else:
 		own_points = own_points[0]
 
-	probation = Probation.objects.filter(recipient=request.user, expirationDate__gt=datetime.now())
-	probation = probation.count() > 0
-
+	probation = Probation.objects.filter(recipient=request.user, expirationDate__gt=datetime.now()).exists()
 	points_form = PiPointsRequestForm()
 
 	context = RequestContext(request, {
 	'current_bone_count': current_bone_count,
-	'bones': bones,
+	'current_bones': current_bones,
+	'expired_bones': expired_bones,
 	'own_points': own_points,
 	'point_records': point_records,
 	'probation': probation,
@@ -74,14 +74,17 @@ def edit_bones(request):
 		View for editing/managing all bones and punishments
 	"""
 
-	all_bones = Bone.objects.all().order_by('dateReceived')
-	all_probations = Probation.objects.all().order_by('dateReceived')
-	bone_edit_history = BoneChangeRecord.objects.all().order_by('dateChangeMade')
+	all_bones = Bone.objects.filter(expirationDate__gt=datetime.now()).order_by('-dateReceived')
+	expired_bones = Bone.objects.filter(expirationDate__lte=datetime.now())
+	all_probations = Probation.objects.filter(expirationDate__gt=datetime.now()).order_by('-dateReceived')
+
+	bone_edit_history = BoneChangeRecord.objects.all().order_by('-dateChangeMade')
 	probation_form = ProbationGivingForm()
 	bone_form = BoneGivingForm()
 
 	context = RequestContext(request, {
-	'all_bones': all_bones,
+	'active_bones': all_bones,
+	'expired_bones': expired_bones,
 	'all_probations': all_probations,
 	'probation_form': probation_form,
 	'bone_form': bone_form,
@@ -90,9 +93,93 @@ def edit_bones(request):
 
 	return render(request, "secure/standards_bones.html", context)
 
-@permission_required('Standards.add_bone', login_url='PubSite.views.permission_denied')
+@permission_required('Standards.change_bone', login_url='PubSite.views.permission_denied')
 def edit_bone(request, bone):
-	pass
+	"""
+		View for editing a single bone.
+	"""
+
+	try:
+		targetBone = Bone.objects.get(pk=bone)
+		expired = targetBone.expirationDate <= datetime.now().date()
+	except:
+		return redirect('PubSite.views.permission_denied')
+
+	if request.method == 'POST':
+		if expired:
+			return redirect('PubSite.views.permission_denied')
+
+		oldReason = targetBone.reason
+		oldPerson = targetBone.bonee
+		oldExpiration = targetBone.expirationDate
+		form = BoneGivingForm(request.POST, instance=targetBone)
+
+		if form.is_valid():
+			bone = form.save()
+			if not bone.bonee == oldPerson:
+				bone.bonee = oldPerson
+				bone.save()
+			record = BoneChangeRecord()
+			record.bone = targetBone
+			record.modifier = request.user
+			record.dateChangeMade = datetime.now()
+			record.previousReason = oldReason
+			record.newReason = targetBone.reason
+			record.previousExpirationDate = oldExpiration
+			record.newExpirationDate = targetBone.expirationDate
+			record.save()
+
+		return redirect('Standards.views.edit_bones')
+	else:
+		bone_history = BoneChangeRecord.objects.filter(bone=targetBone).order_by('-dateChangeMade')
+
+		if not expired:
+			bone_form = BoneGivingForm(instance=targetBone)
+		else:
+			bone_form = None
+
+		context = RequestContext(request, {
+			'bone': targetBone,
+			'expired': expired,
+			'bone_form': bone_form,
+			'bone_history': bone_history
+			})
+
+		return render(request, "secure/standards_edit_bone.html", context)
+
+@permission_required('Standards.delete_bone', login_url='PubSite.views.permission_denied')
+def expire_bone(request, bone):
+	"""
+		View for expiring a single bone.
+	"""
+
+	try:
+		targetBone = Bone.objects.get(pk=bone)
+		expired = targetBone.expirationDate <= datetime.now().date()
+	except:
+		return redirect('PubSite.views.permission_denied')
+
+	if request.method == 'POST':
+		if expired:
+			return redirect('PubSite.views.permission_denied')
+
+		oldExpirationDate = targetBone.expirationDate
+		targetBone.expirationDate = datetime.now()
+		targetBone.save()
+
+		record = BoneChangeRecord()
+		record.bone = targetBone
+		record.modifier = request.user
+		record.dateChangeMade = datetime.now()
+		record.previousReason = targetBone.reason
+		record.newReason = targetBone.reason
+		record.previousExpirationDate = oldExpirationDate
+		record.newExpirationDate = targetBone.expirationDate
+		record.save()
+
+		return redirect('Standards.views.edit_bones')
+	else:
+		return redirect('Standards.views.edit_bones')
 
 @permission_required('Standards.add_bone', login_url='PubSite.views.permission_denied')
 def add_bone(request):
@@ -107,6 +194,16 @@ def add_bone(request):
 			bone.boner = request.user
 			bone.dateReceived = datetime.now()
 			bone.save()
+
+			record = BoneChangeRecord()
+			record.bone = bone
+			record.modifier = request.user
+			record.dateChangeMade = datetime.now()
+			record.previousReason = "CREATED"
+			record.newReason = bone.reason
+			record.previousExpirationDate = bone.expirationDate
+			record.newExpirationDate = bone.expirationDate
+			record.save()
 
 		return redirect('Standards.views.edit_bones')
 	else:
@@ -129,6 +226,21 @@ def add_probation(request):
 		return redirect('Standards.views.edit_bones')
 	else:
 		return redirect('PubSite.views.permission_denied')
+
+@permission_required('Standards.delete_probation', login_url='PubSite.views.permission_denied')
+def end_probation(request, probation):
+	"""
+		View for ending a social probation.
+	"""
+	if request.method == 'POST':
+		probation = Probation.objects.get(pk=probation)
+		probation.expirationDate = datetime.now()
+		probation.save()
+
+		return redirect('Standards.views.edit_bones')
+	else:
+		return redirect('PubSite.views.permission_denied')
+
 
 @permission_required('Standards.add_pipointsrecord', login_url='PubSite.views.permission_denied')
 def edit_points(request):
--- SigmaPi/static/js/secure/standards/standards_bones.js
+++ SigmaPi/static/js/secure/standards/standards_bones.js
@@ -1,3 +1,5 @@
+var changes_open = false;
+var expired_open = false;
 $(document).ready(function() {	
 	$.fn.dataTableExt.oStdClasses["sFilter"] = "ui label black"
 	$.fn.dataTableExt.oStdClasses["sLength"] = "ui label black"
@@ -6,12 +8,21 @@ $(document).ready(function() {
 	$.fn.dataTableExt.oStdClasses["sPagePrevDisabled"] = "ui button disabled"
 	$.fn.dataTableExt.oStdClasses["sPageNextEnabled"] = "ui button active"
 	$.fn.dataTableExt.oStdClasses["sPageNextDisabled"] = "ui button disabled"
-	$('#bones_table').dataTable();
-	$('#bone_history_table').dataTable();
-	$('#probation_table').dataTable();
+	$('#bones_table').dataTable({
+		"bSort": false,
+	});
+	$('#bone_history_table').dataTable({
+		"bSort": false,
+	});
+	$('#probation_table').dataTable({
+		"bSort": false,
+	});
+	$('#expired_bones_table').dataTable({
+		"bSort": false,
+	});
 	$('#bone_datepicker').datepicker();
 	$('#probation_datepicker').datepicker();
-
+	$('#tabs').tabs();
 	$('#levy_bone_button').click(function() {
 		$('#levy_probation_form').slideUp(450);
 		$('#levy_bone_form').slideToggle(450);
