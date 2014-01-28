23301701bfa30ee623906ac23002be262aba6081
Dan Robertson <drobertson@kayak.com>
2014-01-28 15:08:10 -0500

    Scrolling bug on pub pages

--- SigmaPi/static/js/pub/pub_common.js
+++ SigmaPi/static/js/pub/pub_common.js
@@ -1,16 +1,12 @@
 $(document).ready(function(){
-	$('.sub-header').css('margin-top','0px'); //can't do this in the css for some reason.
-
-	var thresh = 100; //The height of the top banner
-	var subHeight = 63; //height of the top banner
-	$(window).scroll(function(e){
-		console.log(subHeight,window.scrollY);
-		if (window.scrollY > thresh) {
-			$('#pub-nav').addClass('fixed');
-			$('.sub-header').css('margin-top',subHeight + 'px');
-		} else {
+	$('#pub-nav').waypoint(function(direction){
+		console.log('waypoint');
+		if (direction === 'up') {
 			$('#pub-nav').removeClass('fixed');
-			$('.sub-header').css('margin-top',0);
+			$('.top-header').removeClass('adjusted');
+		} else {
+			$('#pub-nav').addClass('fixed');
+			$('.top-header').addClass('adjusted');
 		}
 	});
 });
