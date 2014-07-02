c19422d9f8efd800f9607eb8294b0e2dee8d249b
Ryan Baker <ryan.da.baker@gmail.com>
2014-07-01 23:20:30 -0400

    corrects whitespace and small bug with total function

--- SigmaPi/Finance/models.py
+++ SigmaPi/Finance/models.py
@@ -6,42 +6,42 @@ from django.forms import ModelChoiceField
 from datetime import datetime
 
 class CommitteeBudget(models.Model):
-  """
+    """
     Model for the budget of a specific committee
-  """
-  name = models.TextField()
-  start_budget = models.PositiveIntegerField(default=0)
-  current_budget = models.positiveIntegerField(default=0)
-  expenses = models.ForeignKey(Expense)
+    """
+    name = models.TextField()
+    start_budget = models.PositiveIntegerField(default=0)
+    current_budget = models.positiveIntegerField(default=0)
+    expenses = models.ForeignKey(Expense)
 
-  def __unicode__(self):
-    return self.name
+    def __unicode__(self):
+        return self.name
 
-  def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.name
 
-  def total_expenses(self):
-    return sum[e.amount for e in self.expenses]
+    def total_expenses(self):
+        return sum([e.amount for e in self.expenses])
 
-  class Meta:
-    verbose_name = "Budget"
-    verbost_name_plural = "Budgets"
+    class Meta:
+        verbose_name = "Budget"
+        verbost_name_plural = "Budgets"
 
 class Expense(models.Model):
-  """
+    """
     Model for an expense of a committee
-  """
-  amount = models.IntegerField()
-  name = models.TextField()
-  description = models.TextField()
-  source = models.TextField()
+    """
+    amount = models.IntegerField()
+    name = models.TextField()
+    description = models.TextField()
+    source = models.TextField()
 
-  def __unicode__(self):
-    return self.name
+    def __unicode__(self):
+        return self.name
 
-  def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.name
 
-  class Meta:
-    verbose_name = "Expense"
-    verbose_name_plural = "Expenses"
+    class Meta:
+        verbose_name = "Expense"
+        verbose_name_plural = "Expenses"
