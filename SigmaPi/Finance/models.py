590cc1403af42ad16c971e9af53f9991760a395e
Ryan Baker <ryan.da.baker@gmail.com>
2014-07-01 23:16:23 -0400

    updates gitignore and starts creation of finance module

---
+++ SigmaPi/Finance/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django import forms
+from django.forms import ModelForm, Form
+from django.forms import ModelChoiceField
+from datetime import datetime
+
+class CommitteeBudget(models.Model):
+  """
+    Model for the budget of a specific committee
+  """
+  name = models.TextField()
+  start_budget = models.PositiveIntegerField(default=0)
+  current_budget = models.positiveIntegerField(default=0)
+  expenses = models.ForeignKey(Expense)
+
+  def __unicode__(self):
+    return self.name
+
+  def __str__(self):
+    return self.name
+
+  def total_expenses(self):
+    return sum[e.amount for e in self.expenses]
+
+  class Meta:
+    verbose_name = "Budget"
+    verbost_name_plural = "Budgets"
+
+class Expense(models.Model):
+  """
+    Model for an expense of a committee
+  """
+  amount = models.IntegerField()
+  name = models.TextField()
+  description = models.TextField()
+  source = models.TextField()
+
+  def __unicode__(self):
+    return self.name
+
+  def __str__(self):
+    return self.name
+
+  class Meta:
+    verbose_name = "Expense"
+    verbose_name_plural = "Expenses"
